5d0350020125e0a42b4c12689aacb7055ef18c12
Nathan <nathan.wang2@gmail.com>
2017-08-05 21:05:41 -0500

    Added some css as a test and moved the mapjs file

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import 'whatwg-fetch';
-import { Favorites, Test } from '../fetch/fetch';
-import Map from '../map/map'
+// import { Favorites, Test } from '../fetch/fetch';
+import Map from './map/map'
+import './home.css'
 // key=AIzaSyD-KvhfCHz8YbZguRazLIorA7RaTKksga0
 
 
 class Home extends Component {
   constructor(){
     super();
-    this.state = {}
+    this.state = {
+      location: '',
+      restaurant_name: []
+    }
   }
 
 
@@ -22,27 +26,52 @@ class Home extends Component {
         return response.json()
       })
       .then((json) => {
-        this.setState({
-          "restaurant_name": json[0]['restaurant_name']
-        })
+        for (let i = 0; i < json.length; i++) {
+          let joined = this.state.restaurant_name.concat(json[i]['restaurant_name']);
+
+          this.setState({
+            "restaurant_name": joined
+          })
+        }
+        console.log(this.state.restaurant_name.splice(this.state.restaurant_name))
       })
       .catch((ex) =>  {
         console.log('parsing failed', ex)
       })
     }
 
-  render(){
+  Location(event){
+    this.setState({location: event.target.value});
+  }
+
+  handleSubmit(){
+    console.log('This is submitted to get a request of location');
+    console.log(this.state.location);
     console.log(this.state.restaurant_name)
+  }
+
+  render(){
     return(
       <div>
         <h1> Hi Home </h1>
         <h3> Below is information from api  </h3>
           {this.state.restaurant_name}
-          <Map
-            center={{ lat: 30.2672, lng: -97.7431 }}
-            containerElement={<div style={{height: 400+'px'}} /> }
-            mapElement={<div style={{height:400+'px'}} />}
-            />
+          <div className="SearchMap">
+            <form onSubmit={this.handleSubmit.bind(this)}>
+              <label>
+                Address:
+                <input type="text" value={this.state.location} onChange={this.Location.bind(this)} />
+              </label>
+              <input type="submit" value="Submit" />
+            </form>
+            <div className="Map">
+              <Map
+                center={{ lat: 30.2672, lng: -97.7431 }}
+                containerElement={<div style={{height: 100+'%'}} /> }
+                mapElement={<div style={{height:100+'%'}} />}
+                />
+            </div>
+          </div>
       </div>
     )
   }
--- frontend/src/pages/home/map/map.js
+++ frontend/src/pages/home/map/map.js
@@ -9,23 +9,29 @@ class Map extends Component {
     }
   }
 
-  mouseClick(){
-    console.log('mouse click')
+  handleMapClick(event) {
+    const nextMarkers = [
+      ...this.state.markers,
+      {
+        position: event.latLng,
+        defaultAnimation: 2,
+        key: Date.now(), // Add a key property for: http://fb.me/react-warning-keys
+      },
+    ];
+    this.setState({
+      markers: nextMarkers,
+    });
   }
 
-  mapLoaded(){
-    console.log('loaded')
-    this.setState({
-      markers: [{
-      position: {
-        lat: 30.2672,
-        lng: -97.7431,
-      },
-      key: `Taiwan`,
-      defaultAnimation: 2,
-      }]
-    })
-  }
+  // Examples Marker
+  // markers: [{
+  // position: {
+  //   lat: 30.2672,
+  //   lng: -97.7431,
+  // },
+  // key: `Taiwan`,
+  // defaultAnimation: 2,
+  // }]
 
   render(){
     // const markers = this.props.markers || [];
@@ -33,8 +39,7 @@ class Map extends Component {
       <div>
       map is not showing up for some reason
         <GoogleMap
-          ref={this.mapLoaded.bind(this)}
-          onClick={this.mouseClick.bind(this)}
+          onClick={this.handleMapClick.bind(this)}
           defaultZoom={12}
           defaultCenter={this.props.center} >
           {this.state.markers.map((marker, index) => (
