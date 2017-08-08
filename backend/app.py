8e17302f3a151094b5c4ec1999a282a80961ba3b
Nathan <nathan.wang2@gmail.com>
2017-08-07 21:57:09 -0500

    Added status codes and changed response messages

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, jsonify, json
+from flask import Flask, request, jsonify, json, Response
 from flask_sqlalchemy import SQLAlchemy
 from models.model import db, Users, Favorites, UserSchema, FavoritesSchema
 from key import key
 import requests
 from flask_cors import CORS, cross_origin
+from random import randint
 
 
 app = Flask(__name__)
@@ -11,7 +12,6 @@ db.init_app(app)
 CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:admin@localhost/RestaurantRoulette'
-
 
 
 user_schema = UserSchema()
@@ -27,34 +27,49 @@ def register():
         password = request.json['password']
         insertTest = Users(username, password)
         db.session.add(insertTest)
+        print(username, password)
         db.session.commit()
-        return ("Successfully Registered!")
+        return 200, "Success"
     except:
-        return ('Registeration was Not Successful')
+        return 400, "Failed"
 
 
 ############# user API requests ###################
 @app.route('/user', methods=['GET', 'POST', 'DELETE'])
 def user():
     # For profile
-    if (request.method == 'GET'):
-        username = request.headers['username']
-        users = Users.query.filter_by(username=username).all()
-        # Serialize the queryset
-        result = users_schema.dump(users)
-        return jsonify({'username': result.data[0]["username"]})
+    # if (request.method == 'GET'):
+    #     username = request.headers['username']
+    #     users = Users.query.filter_by(username=username).all()
+    #     # Serialize the queryset
+    #     result = users_schema.dump(users)
+    #     # return jsonify({'username': result.data[0]["username"]})
+    #     resMessage = {
+    #         "username": result.data[0]["username"]
+    #     }
+    #     res = jsonify(resMessage)
+    #     resMessage.status_code = 200
+    #     return res
 
     # Checking username and password verification
-    elif (request.method == 'POST'):
+    if (request.method == 'POST'):
         username = request.json['username']
         password = request.json['password']
-        email = Users.query.filter_by(username=username).all()
+        users = Users.query.filter_by(username=username).all()
         result = users_schema.dump(users)
         if (username == result.data[0]["username"]):
             if (password == result.data[0]["password"]):
-                return ("This post request was succesful.")
-
-        return ("This Login Request has failed")
+                resMessage = {
+                    "message": "Success",
+                }
+                res = jsonify(resMessage)
+                res.status_code = 200
+            else:
+                resMessage = {
+                    "message": "Incorrect username or password",
+                }
+                res = jsonify(resMessage)
+                resMessage.status_code = 400
 
     # For Deleting accounts
     # Account for relationship with favorites list
@@ -64,9 +79,19 @@ def user():
             deleteTest = Users.query.filter_by(username=username).first()
             db.session.delete(deleteTest)
             db.session.commit()
-            return ("The delete request was succesful")
-        except Exception ,e:
-            return("Failed Delete Request")
+            resMessage = {
+                "message": "Success",
+            }
+            res = jsonify(resMessage)
+            res.status_code = 200
+        except Exception as e:
+            resMessage = {
+                "message": "Failure",
+            }
+            res = jsonify(resMessage)
+            res.status_code = 400
+
+    return res
 
 ############## favoites API requests ##########################
 # profile or a way to show the list of what they have as favorites
@@ -74,8 +99,7 @@ def user():
 def favorites():
     # Need to be able to list all of the restaurants as a json
     if (request.method == 'GET'):
-
-        #username = request.json['username']
+        username = request.json['username']
         email = Favorites.query.filter_by(username="Email@email.com").all()
         # Initialize a employee list
         favList = []
@@ -106,33 +130,51 @@ def favorites():
             db.session.delete(deleteTest)
             db.session.commit()
             return ("This is a delete request")
-        except Exception ,e:
+        except Exception as e:
             return("Failed Delete Request")
 
 ####################### Google Maps API ################################
-search_url = "https://maps.googleapis.com/maps/api/place/textsearch/json?type=restaurant"
+search_url = "https://maps.googleapis.com/maps/api/place/textsearch/json"
 details_url = "https://maps.googleapis.com/maps/api/place/details/json"
 
-@app.route('/maps/<string:query>')
-def maps(query):
-    search_payload = {"key":key, "query":query}
-    search_req = requests.get(search_url, params=search_payload)
-    search_json = search_req.json()
-    dictLength = len(search_json)
-    RestaurantNames = [search_json["results"][item]['name'] for item in range(0,dictLength)]
-    # return (jsonify(names = RestaurantNames))
-    # return (jsonify(search_json))
+@app.route('/maps')
+def maps():
+    params = {
+        'radius': request.args.get('radius') or '8000',
+        'location': request.args.get('location')
+    }
 
-    place_id = search_json["results"][0]["place_id"]
+    test = {
+     'query': request.args.get('location'),
+     'radius': request.args.get('radius') or '8000',
+     'type': 'restaurant',
+     'key': key
+    }
+    ########BY DEFAULT IT WILL SEND A LOT OF PLACES!!!! NO FIX YET BY GOOGLE
+    search_json = requests.get(search_url, params=test).json()
+    results = {}
+    ## Should limit to 5 times to limit the details api call
 
-    details_payload = {"key":key, "placeid":place_id}
-    details_resp = requests.get(details_url, params=details_payload)
-    details_json = details_resp.json()
+    # for index, restaurant in enumerate(search_json["results"]):
+    flag = True
+    while flag:
+        index = randint(0, len(search_json["results"])-1)
+        restaurant = search_json['results'][index]
+        if restaurant['opening_hours']['open_now']:
+            results['location'] = restaurant['geometry']['location']
+            results['name'] = restaurant['name']
+            results['address'] = restaurant['formatted_address']
+            # Getting Phone Numbers
+            details_payload = {"key": key, "placeid": restaurant['place_id']}
+            details_resp = requests.get(details_url, params=details_payload)
+            details_json = details_resp.json()
+            results['phone'] = details_json['result']['formatted_phone_number']
+            flag = False
 
-    return(jsonify(details_json))
-    url = details_json["result"]["url"]
-    # return jsonify({'result' : url})
-
+    resMessage = results
+    res = jsonify(resMessage)
+    res.status_code = 200
+    return res
 
 
 if __name__ == '__main__':
